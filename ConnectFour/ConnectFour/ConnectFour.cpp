9e8a0a3da748718a092a7549f1051a07434438e1
Matt <killzone207@gmail.com>
2015-03-31 13:10:16 -0400

    Created initialization + defines header file

--- ConnectFour/ConnectFour/ConnectFour.cpp
+++ ConnectFour/ConnectFour/ConnectFour.cpp
@@ -1,13 +1,48 @@
 #include <iostream>
+#include <vector>
+#include "defines.h"
 
 using namespace std;
 
-int main(int argc, char** argv){
+/*	 0 1 2 3 4 5 6	  
+  0	|_|_|_|_|_|_|_|
+  1	|_|_|_|_|_|_|_|
+  2	|_|_|_|_|_|_|_|
+  3	|_|_|_|_|_|_|_|
+  4	|_|_|_|_|_|_|_|
+  5	|_|_|_|_|_|_|_|
+*/
 
-	cout << "Hello world!" << endl;
+int main(int argc, char** argv) {
 
-	cin.get();
-	
-	return 0;
+// variables declaration
+	int i, j, k;
+	int board[ROW][COL];
+	char init_input;
+	int player;
+
+// initializations
+	for (i = 0; i < ROW; i++) {
+		for (j = 0; j < COL; j++) {
+			board[i][j] = 0;
+		}
+	}
+
+// game start
+	cout << "~~~~~****~~~~~ Welcome to ConnectFour! ~~~~~****~~~~~" << endl << endl
+		<< "Please choose to be Red or Black.  (enter 'R' or 'B')" << endl;
+		
+	while (player == INVALID)
+		init_input = cin.get();
+		if (init_input == 'R' || init_input == 'r') {
+			player = RED;
+		}
+		else if (init_input == 'B' || init_input == 'b') {
+			player = BLACK;
+		}
+		else {
+			player = INVALID;
+			cout << "Incorrect input.  Please enter 'R' or 'B'" << endl;
+		}
 
 }
---
+++ ConnectFour/ConnectFour/defines.h
@@ -0,0 +1,7 @@
+#define ROW 6
+#define COL 7
+
+#define RED 1
+#define BLACK 0
+
+#define INVALID -99
